65c6176ea9222ce4ed71c52e0ddab3c3a3f709be
George <geotsi66@gmail.com>
2025-09-03 23:43:51 +0300

    added footer code

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <div className="bg-dark">
+
+                </div>
+            </footer>
+        </>
+    )
+}
