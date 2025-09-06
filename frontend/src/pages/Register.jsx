ee6f3f96f73114330b12c1bcf7709b55b0acc87d
George <geotsi66@gmail.com>
2025-09-06 12:42:38 +0300

    begun creating form in Register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,9 +2,18 @@
 function Register() {
     return (
         <>
-            <main>
-                <h1>Register</h1>
-            </main>
+            <div className="card shadow-lg rounded-4 bg-dark text-white">
+                <div className="card-header text-white rounded-top-4">
+                    <h3>Register a new account</h3>
+                </div>
+                <form> 
+                    <div className="card-body">
+                        <div className="mb-3">
+
+                        </div>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
