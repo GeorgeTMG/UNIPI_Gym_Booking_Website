42f3903919f4d24ca1e42dc7aba03714c1d659ee
George <geotsi66@gmail.com>
2025-09-07 23:28:21 +0300

    finished registration form

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,13 +1,13 @@
 import {useNavigate} from 'react-router-dom';  
 import { NavLink } from "react-router-dom";
 
-function Navigation() {
+export default function Navigation() {
     const navigate = useNavigate();
 
     return (
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark p-3">
             <div className="container">
-                <a className="navbar-brand fw-bold fs-2 me-5" href="#">Gym Booking</a>
+                <a className="navbar-brand fw-bold fs-2 me-5">Gym Booking</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
@@ -37,5 +37,3 @@ function Navigation() {
         </nav>
     );
 };
-
-export default Navigation;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,21 +1,54 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-function Register() {
+export default function Register() {
+
+    const onRegisterClick = () => {
+        console.log("TEST")
+    }
+
     return (
         <>
-            <div className="card shadow-lg rounded-4 bg-dark text-white">
-                <div className="card-header text-white rounded-top-4">
-                    <h3>Register a new account</h3>
-                </div>
-                <form> 
-                    <div className="card-body">
-                        <div className="mb-3">
-
+            <div className="container py-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card shadow-lg rounded-4">
+                            <div className="card-header text-white bg-dark text-white rounded-top-4">
+                                <h3 className="fw-bold">Register a new account</h3>
+                            </div>
+                            <Form> 
+                                <div className="card-body rounded-4 p-4">
+                                    <div className="container px-5">
+                                        <Form.Group className="mb-4">
+                                            <Form.Label>First Name</Form.Label>
+                                            <Form.Control type="textarea" placeholder="John" />
+                                        </Form.Group>
+                                        <Form.Group className="mb-4">
+                                            <Form.Label>Last Name</Form.Label>
+                                            <Form.Control type="textarea" placeholder="Doe" />
+                                        </Form.Group>
+                                        <Form.Group className="mb-4" controlId="exampleForm.ControlInput1">
+                                            <Form.Label>Email address</Form.Label>
+                                            <Form.Control type="email" placeholder="name@example.com" />
+                                        </Form.Group>
+                                        <Form.Group className="mb-4" controlId="formPlaintextPassword">
+                                            <Form.Label>Password</Form.Label>
+                                            <Form.Control type="password" placeholder="Enter a password with 8-12 characters" />
+                                        </Form.Group>
+                                    </div>
+                                    <div className="d-flex" id='register-btn'>
+                                        <Button variant="primary" className="fs-5 fw-bold w-100" onClick={() => onRegisterClick}>Register</Button>
+                                    </div>
+                                    <div>
+                                        <p className='mb-0 mt-3'>Already have an account?</p>
+                                        <Button variant="success" className="fs-5 fw-bold">Login</Button>
+                                    </div>
+                                </div>
+                            </Form>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
         </>
     )
 }
-
-export default Register
