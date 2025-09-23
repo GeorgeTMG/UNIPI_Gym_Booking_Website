b315d745bd50ec5d03ce60863c0e887ce5baf9fd
George <geotsi66@gmail.com>
2025-09-23 23:50:22 +0300

    added login route to backend (need to add actions to login page)

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 import User from "./models/User.js"
 
@@ -16,12 +18,14 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
+// Register
 app.post("/register", async (req, res) => {
   try {
     const { firstName, lastName, username, email, password, country, city, address } = req.body;
 
     // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const salt = await bcrypt.genSalt(10); // generate random string to add to password
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
       firstName,
@@ -42,6 +46,46 @@ app.post("/register", async (req, res) => {
   } catch (err) {
     // Respond with error
     res.status(500).json({ error: "Registration failed" });
+  }
+});
+
+// Login
+app.post("/api/auth/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find user
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ message: "Invalid username or password" });
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.passwordHash);
+    if (!isMatch) return res.status(400).json({ message: "Invalid username or password" });
+
+    // If account not approved yet
+    if (user.role === "pending") {
+      return res.status(403).json({ message: "Your account is awaiting admin approval." });
+    }
+
+    // Create JWT
+    const token = jwt.sign(
+      { id: user._id, role: user.role }, 
+      process.env.JWT_SECRET, 
+      { expiresIn: "1h" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        role: user.role
+      }
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
   }
 });
 
